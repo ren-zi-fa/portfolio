afa26d29c94c54a1d0d7b2950b9ac83edafd4c6e
ren-zi-fa <renzifebriandika923@gmail.com>
2024-08-05 11:21:09 +0700

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -8,35 +8,38 @@ import About from "./pages/about.jsx";
 import Blog from "./pages/blog.jsx";
 import MainLayouts from "./components/Layouts/MainLayouts.jsx";
 import DarkModeContextProvider from "./hooks/DarkMode.jsx";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <MainLayouts>
-        <Home />
-      </MainLayouts>
-    ),
-    errorElement: <ErrorPageNotfound />,
-  },
-  {
-    path: "/about",
-    element: (
-      <MainLayouts>
-        <About />
-      </MainLayouts>
-    ),
-    errorElement: <ErrorPageNotfound />,
-  },
-  {
-    path: "/blog",
-    element: (
-      <MainLayouts>
-        <Blog />
-      </MainLayouts>
-    ),
-    errorElement: <ErrorPageNotfound />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <MainLayouts>
+          <Home />
+        </MainLayouts>
+      ),
+      errorElement: <ErrorPageNotfound />,
+    },
+    {
+      path: "/about",
+      element: (
+        <MainLayouts>
+          <About />
+        </MainLayouts>
+      ),
+      errorElement: <ErrorPageNotfound />,
+    },
+    {
+      path: "/blog",
+      element: (
+        <MainLayouts>
+          <Blog />
+        </MainLayouts>
+      ),
+      errorElement: <ErrorPageNotfound />,
+    },
+  ],
+  { basename: import.meta.env.DEV ? "/" : "/react-vite-gh-pages/" }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
